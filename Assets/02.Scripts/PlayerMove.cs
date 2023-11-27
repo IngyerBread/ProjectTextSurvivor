aafca32e375719efde274df4e5c311b4f6b5fc5d
IngyerBread <139726037+IngyerBread@users.noreply.github.com>
2023-11-27 17:48:04 +0900

    기초2

--- Assets/02.Scripts/PlayerMove.cs
+++ Assets/02.Scripts/PlayerMove.cs
@@ -5,12 +5,35 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     [Header("Player Control")]
-    [SerializeField] private float playerMoveSpeed = 1;
+    [SerializeField] private float basicMaxHp = 100f;
+    [SerializeField] private float basicAttackPower = 100f;
+    [SerializeField] private float basicMoveSpeed = 100f;
+    
+
+    [Header("Connect Component")]
     [SerializeField] private Rigidbody2D playerRb;
+    [SerializeField] private GameObject playerGun;
+
+    private float _playerHp;
+    private float _playerMoveSpeed;
+    private bool playerLookToggle = false;
+    private Vector3 playerMoveLook;
+
+    private void Start()
+    {
+        _playerHp = basicMaxHp;
+        _playerMoveSpeed = basicMoveSpeed;
+    }
 
     void Update()
     {
-        Vector2 playerMoveVector = Vector2.zero;
+        PlayerMoveControl();
+        playerGun.transform.LookAt(PlayerLookSystem());
+    }
+
+    private void PlayerMoveControl()
+    {
+        Vector3 playerMoveVector = Vector3.zero;
 
         if (Input.GetKey(KeyCode.W))
         {
@@ -29,6 +52,70 @@ public class PlayerMove : MonoBehaviour
             playerMoveVector.x = 1;
         }
 
-        playerRb.velocity = playerMoveVector.normalized * playerMoveSpeed;
+        playerMoveLook = playerMoveVector;
+
+        playerRb.velocity = playerMoveVector.normalized * _playerMoveSpeed;
+    }
+
+    private Vector3 PlayerLookSystem()
+    {
+        Vector3 playerLookVector;
+
+        if (Input.GetMouseButton(0))
+        {
+            playerLookVector = MouseLookMethod();
+
+            return playerLookVector;
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (playerLookToggle == false)
+            {
+                playerLookToggle = true;
+            }
+
+            else if (playerLookToggle == true)
+            {
+                playerLookToggle = false;
+            }
+        }
+
+        if (playerLookToggle)
+        {
+            playerLookVector = MouseLookMethod();
+
+            return playerLookVector;
+        }
+
+        return playerMoveLook;
+    }
+
+    private Vector3 MouseLookMethod()
+    {
+        Vector3 result;
+        Vector3 mousePos = Input.mousePosition;
+
+        result = Camera.main.ScreenToWorldPoint(mousePos);
+        return result;
+    }
+    /// <summary>
+    /// 플레이어 캐릭터가 바라보는(사격방향)의 우선순위는
+    /// 1순위 좌클
+    /// 2순위 우클(토글)
+    /// 3순위 키보드 (최근 이동한 방향을 바라봄)
+    /// </summary>
+    /// <param name="damage"></param>
+
+    public void PlayerDamaged(float damage)
+    {
+
+    }
+
+    private float PercentCalculator(float value, float percent)
+    {
+        float resultValue = value / 100 * percent;
+
+        return resultValue;
     }
 }
